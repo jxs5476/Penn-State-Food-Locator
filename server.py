72a989a1379f2578b021e781fe18834c55f3f367
jxs5476 <jxs5476@psu.edu>
2020-12-08 19:15:25 -0500

    created text box, moved html

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import *
 from flask_cors import CORS
 
-#import food location py file
+# import food location py file
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+if __name__ == '__main__':
+    app.run(port=8000)
