a76fef2da6e674b1574bf75e3a8a16f71a8f07e1
jxs5476 <jxs5476@psu.edu>
2020-12-08 19:36:25 -0500

    finished? json

--- server.py
+++ server.py
@@ -12,5 +12,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/search', methods=["POST"])
+def get_query():
+    query = request.get_json()
+
+
 if __name__ == '__main__':
     app.run(port=8000)
